b32180488bbc880d9179c2b2e60bdbc00c2d0e26
Felipe <felipesc2007@gmail.com>
2026-08-24 23:49:13 -0300

    new update

--- src/main/java/com/felipe/streaming/controller/PageController.java
+++ src/main/java/com/felipe/streaming/controller/PageController.java
@@ -1,9 +1,13 @@
 package com.felipe.streaming.controller;
 
+import com.felipe.streaming.model.MediaFile;
 import com.felipe.streaming.service.MediaLibraryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class PageController {
@@ -18,5 +22,17 @@ public class PageController {
 
         model.addAttribute("videos", mediaLibraryService.listAll());
         return "library";
+    }
+
+    @GetMapping("/watch/{id}")
+    public String route(@PathVariable String id, Model model) {
+        Optional<MediaFile> media =  mediaLibraryService.findById(id);
+
+        if (media.isEmpty()){
+            return "redirect:/";
+        }
+
+        model.addAttribute("video", media.get());
+        return "watch";
     }
 }
